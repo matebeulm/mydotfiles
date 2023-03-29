40c163ab512d184162f18bf3fa43758ef788196f
Matthias Eberhardt <m.ebe@omtsys.com>
2023-03-29 17:51:26 +0200

    current c++ function in lualine

--- nvim/lua/plugins/lsp-support.lua
+++ nvim/lua/plugins/lsp-support.lua
@@ -1,28 +1,36 @@
 return {
-	-- Language Support
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v1.x',
-	dependencies = {
-		-- LSP Support
-		{ 'neovim/nvim-lspconfig' },         -- Required
-		{ 'williamboman/mason.nvim' },       -- Optional
-		{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  -- Language Support
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  dependencies = {
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },           -- Required
+    { 'williamboman/mason.nvim' },         -- Optional
+    { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		-- Autocompletion
-		{ 'hrsh7th/nvim-cmp' },     -- Required
-		{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-		{ 'hrsh7th/cmp-buffer' },   -- Optional
-		{ 'hrsh7th/cmp-path' },     -- Optional
-		{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
-		{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },       -- Required
+    { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+    { 'hrsh7th/cmp-buffer' },     -- Optional
+    { 'hrsh7th/cmp-path' },       -- Optional
+    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+    { 'hrsh7th/cmp-nvim-lua' },   -- Optional
 
-		-- Snippets
-		{ 'L3MON4D3/LuaSnip' },         -- Required
-		{ 'rafamadriz/friendly-snippets' }, -- Optional
-	},
-	config = function()
-		require("lsp-zero").preset("recommended")
-		require("lsp-zero").nvim_workspace()
-		require("lsp-zero").setup()
-	end
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },           -- Required
+    { 'rafamadriz/friendly-snippets' }, -- Optional
+
+    -- navigation
+    { "SmiteshP/nvim-navic" },
+  },
+  config = function()
+    require("lsp-zero").preset("recommended")
+    require("lsp-zero").nvim_workspace()
+    require("lsp-zero").setup()
+    require("lspconfig").clangd.setup({
+      on_attach = function(client, bufnr)
+        require("nvim-navic").attach(client, bufnr)
+      end
+    })
+  end
 }
