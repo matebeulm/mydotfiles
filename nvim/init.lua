87f3741e1b6e395444518a40032f4ecb2207fd69
Matthias Eberhardt <m.ebe@omtsys.com>
2023-05-18 11:58:59 +0200

    new config based on alpha2phi

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,18 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.options")
 require("config.lazy")
+
+-- if nvim called without arguments
+if vim.fn.argc(-1) == 0 then
+  vim.api.nvim_create_autocmd("User", {
+    group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+      require "config.autocmds"
+      require "config.keymaps"
+    end,
+  })
+else
+  require "config.autocmds"
+  require "config.keymaps"
+end
+
