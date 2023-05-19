9fd81280d7e54d80aa3316fe52ab4ac74455d3a4
Matthias Eberhardt <m.ebe@omtsys.com>
2023-05-19 07:46:27 +0200

    wip cosmetics

--- nvim/lua/base/coding.lua
+++ nvim/lua/base/coding.lua
@@ -1,7 +1,7 @@
 return {
   {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
+    'kylechui/nvim-surround',
+    event = 'VeryLazy',
     opts = {},
   },
   {
@@ -14,37 +14,39 @@ return {
     },
     config = function(_, _)
       local opts = {
-        ignore = "^$",
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        ignore = '^$',
+        pre_hook = require(
+          'ts_context_commentstring.integrations.comment_nvim'
+        ).create_pre_hook(),
       }
-      require("Comment").setup(opts)
+      require('Comment').setup(opts)
     end,
   },
   {
-    "andymass/vim-matchup",
-    event = { "BufReadPost" },
+    'andymass/vim-matchup',
+    event = { 'BufReadPost' },
     config = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+      vim.g.matchup_matchparen_offscreen = { method = 'popup' }
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
+      'hrsh7th/cmp-nvim-lsp',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
     },
     config = function()
-      local cmp = require "cmp"
-      local luasnip = require "luasnip"
-      local compare = require "cmp.config.compare"
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      local compare = require('cmp.config.compare')
       local source_names = {
-        nvim_lsp = "(LSP)",
-        luasnip = "(Snippet)",
-        buffer = "(Buffer)",
-        path = "(Path)",
+        nvim_lsp = '(LSP)',
+        luasnip = '(Snippet)',
+        buffer = '(Buffer)',
+        path = '(Path)',
       }
       local duplicates = {
         buffer = 1,
@@ -54,15 +56,20 @@ return {
       }
       local has_words_before = function()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+        return col ~= 0
+          and vim.api
+              .nvim_buf_get_lines(0, line - 1, line, true)[1]
+              :sub(col, col)
+              :match('%s')
+            == nil
       end
 
       -- If you want insert `(` after select function or method item
       local cmp_autopairs = require('nvim-autopairs.completion.cmp')
       cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-      cmp.setup {
+      cmp.setup({
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = 'menu,menuone,noinsert',
         },
         sorting = {
           priority_weight = 2,
@@ -79,25 +86,31 @@ return {
         },
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
-        mapping = cmp.mapping.preset.insert {
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping {
-            i = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping({
+            i = cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = false,
+            }),
             c = function(fallback)
               if cmp.visible() then
-                cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
+                cmp.confirm({
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = false,
+                })
               else
                 fallback()
               end
             end,
-          },
-          ["<C-j>"] = cmp.mapping(function(fallback)
+          }),
+          ['<C-j>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -108,11 +121,11 @@ return {
               fallback()
             end
           end, {
-            "i",
-            "s",
-            "c",
+            'i',
+            's',
+            'c',
           }),
-          ["<C-k>"] = cmp.mapping(function(fallback)
+          ['<C-k>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -121,20 +134,20 @@ return {
               fallback()
             end
           end, {
-            "i",
-            "s",
-            "c",
+            'i',
+            's',
+            'c',
           }),
-        },
-        sources = cmp.config.sources {
+        }),
+        sources = cmp.config.sources({
           -- { name = "nvim_lsp_signature_help", group_index = 1 },
-          { name = "nvim_lsp", group_index = 1 },
-          { name = "luasnip", group_index = 1 },
-          { name = "buffer", group_index = 2 },
-          { name = "path", group_index = 2 },
-        },
+          { name = 'nvim_lsp', group_index = 1 },
+          { name = 'luasnip', group_index = 1 },
+          { name = 'buffer', group_index = 2 },
+          { name = 'path', group_index = 2 },
+        }),
         formatting = {
-          fields = { "kind", "abbr", "menu" },
+          fields = { 'kind', 'abbr', 'menu' },
           format = function(entry, item)
             local duplicates_default = 0
             item.menu = source_names[entry.source.name]
@@ -142,23 +155,23 @@ return {
             return item
           end,
         },
-      }
+      })
     end,
   },
   {
-    "L3MON4D3/LuaSnip",
+    'L3MON4D3/LuaSnip',
     dependencies = {
       {
-        "rafamadriz/friendly-snippets",
+        'rafamadriz/friendly-snippets',
         config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
+          require('luasnip.loaders.from_vscode').lazy_load()
         end,
       },
     },
-    build = "make install_jsregexp",
+    build = 'make install_jsregexp',
     opts = {
       history = true,
-      delete_check_events = "TextChanged",
+      delete_check_events = 'TextChanged',
     },
     -- stylua: ignore
     keys = {
@@ -173,7 +186,7 @@ return {
       { "<C-k>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
     config = function(_, opts)
-      require("luasnip").setup(opts)
+      require('luasnip').setup(opts)
     end,
   },
   {
@@ -213,4 +226,3 @@ return {
     end,
   },
 }
-
